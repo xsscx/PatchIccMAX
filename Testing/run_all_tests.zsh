33e25b5e7e688e6bd229c0e3fc6a1048a336369d
David Hoyt <dhoyt@hoyt.net>
2024-09-12 10:00:38 -0400

    Create run_all_tests.zsh

---
+++ Testing/run_all_tests.zsh
@@ -0,0 +1,23 @@
+echo "Creating Profiles..."
+./CreateAllProfiles.sh
+./run_tests.zsh
+echo "cd to CalcTest"
+cd CalcTest
+./checkInvalidProfiles.sh
+./run_tests.zsh
+echo "cd to HDR"
+cd ../HDR
+./mkprofiles.sh
+echo "cd to Overprint"
+cd ../Overprint
+./RunTests.sh
+echo "cd to Display"
+cd ../Display
+./RunProtoTests.sh
+echo "cd mcs"
+cd ../mcs
+./updateprevWithBkgd.sh
+./updateprev.sh
+echo "cd Testing"
+cd ../
+echo "done..."
