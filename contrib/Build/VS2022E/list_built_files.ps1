7e3d7bdeab63de7719725e5c6af5e255b43a6d7a
David Hoyt <dhoyt@hoyt.net>
2024-09-20 08:05:03 -0700

    Update list_built_files.ps1
    Get-ChildItem -Path "Build/" -Recurse -Include *.exe, *.dll, *.a, *.lib | Select-Object FullName, LastWriteTime, CreationTime
    Get-ChildItem -Path "IccProfLib/" -Recurse -Include *.exe, *.dll, *.a, *.lib | Select-Object FullName, LastWriteTime, CreationTime
    Get-ChildItem -Path "IccXML/" -Recurse -Include *.exe, *.dll, *.a, *.lib | Select-Object FullName, LastWriteTime, CreationTime
    Get-ChildItem -Path "Testing/" -Recurse -Include *.exe, *.dll, *.a, *.lib | Select-Object FullName, LastWriteTime, CreationTime
    Get-ChildItem -Path "Tools/" -Recurse -Include *.exe, *.dll, *.a, *.lib | Select-Object FullName, LastWriteTime, CreationTime

--- contrib/Build/VS2022E/list_built_files.ps1
+++ contrib/Build/VS2022E/list_built_files.ps1
@@ -1,1 +1,5 @@
-Get-ChildItem -Path "." -Recurse -Include *.exe, *.dll, *.a, *.lib | Select-Object FullName, LastWriteTime
+Get-ChildItem -Path "Build/" -Recurse -Include *.exe, *.dll, *.a, *.lib | Select-Object FullName, LastWriteTime, CreationTime
+Get-ChildItem -Path "IccProfLib/" -Recurse -Include *.exe, *.dll, *.a, *.lib | Select-Object FullName, LastWriteTime, CreationTime
+Get-ChildItem -Path "IccXML/" -Recurse -Include *.exe, *.dll, *.a, *.lib | Select-Object FullName, LastWriteTime, CreationTime
+Get-ChildItem -Path "Testing/" -Recurse -Include *.exe, *.dll, *.a, *.lib | Select-Object FullName, LastWriteTime, CreationTime
+Get-ChildItem -Path "Tools/" -Recurse -Include *.exe, *.dll, *.a, *.lib | Select-Object FullName, LastWriteTime, CreationTime
