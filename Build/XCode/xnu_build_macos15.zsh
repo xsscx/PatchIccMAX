4b02b5b38787e5007d6e051502b27bd36e261674
David Hoyt <dhoyt@hoyt.net>
2024-10-08 12:21:41 -0400

    Update xnu_build_macos15.zsh
    cd ../../../IccXML/CmdLine/IccFromXml
    xcodebuild -target iccFromXml -configuration "$config" -arch x86_64 clean
    xcodebuild -target iccFromXml -configuration "$config" -arch x86_64 -sdk macosx15.0 \
    ARCHS="x86_64" VALID_ARCHS="x86_64"
    cp build/$config/IccFromXml ../../../Testing
    
    cd ../../../IccXML/CmdLine/IccToXml
    xcodebuild -target IccToXml -configuration "$config" -arch x86_64 clean
    xcodebuild -target IccToXml -configuration "$config" -arch x86_64 -sdk macosx15.0 \
    ARCHS="x86_64" VALID_ARCHS="x86_64"
    cp build/$config/IccToXml ../../../Testing

--- Build/XCode/xnu_build_macos15.zsh
+++ Build/XCode/xnu_build_macos15.zsh
@@ -46,28 +46,26 @@ cd ../IccSpecSepToTiff
 cp ../IccApplyProfiles/TiffImg.* .
 xcodebuild -target IccSpecSepToTiff -configuration "$config" -arch x86_64 clean
 xcodebuild -target IccSpecSepToTiff -configuration "$config" -arch x86_64 \
-LIBRARY_SEARCH_PATHS="../../../IccProfLib/build/$config/  /usr/local/opt/jpeg/lib /usr/local/opt/libtiff/lib" \
-OTHER_LDFLAGS="-lIccProfLib -ljpeg -ltiff -llzma -lz"
+LIBRARY_SEARCH_PATHS="../../../IccProfLib/build/$config /usr/local/opt/jpeg/lib /usr/local/opt/libtiff/lib /usr/local/opt/webp/lib /usr/local/opt/zstd/lib" \
+OTHER_LDFLAGS="-lIccProfLib -ljpeg -ltiff -lwebp -llzma -lz -lzstd"
 cp build/$config/IccSpecSepToTiff ../../../Testing
 
 cd ../IccTiffDump
 cp ../IccApplyProfiles/TiffImg.* .
 xcodebuild -target IccTiffDump -configuration "$config" -arch x86_64 clean
 xcodebuild -target IccTiffDump -configuration "$config" -arch x86_64 \
-LIBRARY_SEARCH_PATHS="../../../IccProfLib/build/$config/  /usr/local/opt/jpeg/lib /usr/local/opt/libtiff/lib" \
-OTHER_LDFLAGS="-lIccProfLib -ljpeg -ltiff -llzma -lz"
+LIBRARY_SEARCH_PATHS="../../../IccProfLib/build/$config /usr/local/opt/jpeg/lib /usr/local/opt/libtiff/lib /usr/local/opt/webp/lib /usr/local/opt/zstd/lib" \
+OTHER_LDFLAGS="-lIccProfLib -ljpeg -ltiff -lwebp -llzma -lz -lzstd"
 cp build/$config/IccTiffDump ../../../Testing
 
 cd ../../../IccXML/CmdLine/IccFromXml
 xcodebuild -target iccFromXml -configuration "$config" -arch x86_64 clean
-xcodebuild -target iccFromXml -configuration "$config" -arch x86_64 \
-LIBRARY_SEARCH_PATHS="../../../IccProfLib/build/$config/  /usr/local/opt/jpeg/lib /usr/local/opt/libtiff/lib" \
-OTHER_LDFLAGS="-lIccProfLib -ljpeg -ltiff -llzma -lz"
-cp $config/IccFromXml ../../../Testing
+xcodebuild -target iccFromXml -configuration "$config" -arch x86_64 -sdk macosx15.0 \
+ARCHS="x86_64" VALID_ARCHS="x86_64"
+cp build/$config/IccFromXml ../../../Testing
 
 cd ../../../IccXML/CmdLine/IccToXml
 xcodebuild -target IccToXml -configuration "$config" -arch x86_64 clean
-xcodebuild -target IccToXml -configuration "$config" -arch x86_64 \
-LIBRARY_SEARCH_PATHS="../../../IccProfLib/build/$config/  /usr/local/opt/jpeg/lib /usr/local/opt/libtiff/lib" \
-OTHER_LDFLAGS="-lIccProfLib -ljpeg -ltiff -llzma -lz"
-cp $config/IccToXml ../../../Testing
+xcodebuild -target IccToXml -configuration "$config" -arch x86_64 -sdk macosx15.0 \
+ARCHS="x86_64" VALID_ARCHS="x86_64"
+cp build/$config/IccToXml ../../../Testing
