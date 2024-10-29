57f0dfec21f210edda14b58fc31e43ce8ac5942b
David Hoyt <dhoyt@hoyt.net>
2024-10-28 21:14:42 -0400

    Update static_build_cli_production.ps1
    $env:VSCMD_ARG_HOST_ARCH = "x64"
    $env:VSCMD_ARG_TGT_ARCH = "x64"

--- contrib/Build/VS2022E/static_build_cli_production.ps1
+++ contrib/Build/VS2022E/static_build_cli_production.ps1
@@ -10,6 +10,8 @@
 
 # Start of Script
 Write-Host "============================= Starting DemoIccMAX Static Branch Build =============================" -ForegroundColor Green
+$env:VSCMD_ARG_HOST_ARCH = "x64"
+$env:VSCMD_ARG_TGT_ARCH = "x64"
 Write-Host "Copyright (c) 2024 The International Color Consortium. All rights reserved." -ForegroundColor Green
 Write-Host "Author: David Hoyt written for ICC color.org and DemoIccMAX Project" -ForegroundColor Green
 Write-Host "Creating 'testing' directory..." -ForegroundColor Yellow
