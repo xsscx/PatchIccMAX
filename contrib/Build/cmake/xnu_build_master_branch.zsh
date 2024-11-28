93c18fe5083f70f5cf9839b71c7f094a6f542517
David Hoyt <dhoyt@hoyt.net>
2024-11-27 21:34:31 -0500

    Update xnu_build_master_branch.zsh
    nlohmann-json

--- contrib/Build/cmake/xnu_build_master_branch.zsh
+++ contrib/Build/cmake/xnu_build_master_branch.zsh
@@ -52,7 +52,7 @@ run_and_log echo "master branch checked out."
 
 # Step 4: Installing Dependencies
 print_banner "Step 4: Installing Dependencies, you will be prompted for the sudo password to continue..."
-brew install libtiff libxml2 wxwidgets libxml2 libtiff|| { echo "Error: Failed to install dependencies via Homebrew. Exiting."; exit 1; }
+brew install nlohmann-json libtiff libxml2 wxwidgets libxml2 libtiff|| { echo "Error: Failed to install dependencies via Homebrew. Exiting."; exit 1; }
 
 # Step 5: Build
 print_banner "Step 5: Starting Build...."
