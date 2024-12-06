0b6f09081a75f0d0b2b63c18a15a4a63646e18b6
David Hoyt <dhoyt@hoyt.net>
2024-12-06 07:54:51 -0500

    Update build_development.ps1
    Write-Host "Installing required libraries (libxml2, tiff, wxwidgets) for x64-windows-static..."
    .\vcpkg.exe install egl-registry:x64-windows-static vcpkg-cmake:x64-windows-static vcpkg-cmake-config:x64-windows-static nlohmann-json:x64-windows nlohmann-json:x64-windows-static libxml2:x64-windows tiff:x64-windows wxwidgets:x64-windows libxml2:x64-windows tiff:x64-windows wxwidgets:x64-windows libxml2:x64-windows-static tiff:x64-windows-static wxwidgets:x64-windows-static

--- contrib/Build/VS2022E/build_development.ps1
+++ contrib/Build/VS2022E/build_development.ps1
@@ -41,7 +41,8 @@ if (-not (Test-Path ".\vcpkg.exe")) {
     .\bootstrap-vcpkg.bat
 }
 .\vcpkg.exe integrate install
-.\vcpkg.exe install libxml2:x64-windows tiff:x64-windows wxwidgets:x64-windows libxml2:x64-windows tiff:x64-windows wxwidgets:x64-windows libxml2:x64-windows-static tiff:x64-windows-static wxwidgets:x64-windows-static
+Write-Host "Installing required libraries (libxml2, tiff, wxwidgets) for x64-windows-static..."
+.\vcpkg.exe install egl-registry:x64-windows-static vcpkg-cmake:x64-windows-static vcpkg-cmake-config:x64-windows-static nlohmann-json:x64-windows nlohmann-json:x64-windows-static libxml2:x64-windows tiff:x64-windows wxwidgets:x64-windows libxml2:x64-windows tiff:x64-windows wxwidgets:x64-windows libxml2:x64-windows-static tiff:x64-windows-static wxwidgets:x64-windows-static
 
 # Clone DemoIccMAX repository
 cd $baseDir
