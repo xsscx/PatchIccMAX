911beb923c756e5dc6aa725eda18451edefbfd3b
David Hoyt <dhoyt@hoyt.net>
2025-03-15 12:50:20 -0400

    Update build_asan_alt.ps1

--- contrib/Build/VS2022C/build_asan_alt.ps1
+++ contrib/Build/VS2022C/build_asan_alt.ps1
@@ -1,7 +1,7 @@
 # ========================== PatchIccMAX ASAN ALT Build Script for VS2022 Community ==========================
 # © 2024-2025 David H Hoyt LLC. All rights reserved.
 #
-# Date: 25-FEB-2025 1522 by David Hoyt
+# Date: 15-MAR-2025 1245 EDT by David Hoyt
 #
 #
 #
@@ -62,7 +62,8 @@ Log-Message "https://srd.cx"
 Log-Message "======================================================================="
 set MSBUILDDEBUGENGINE=1
 # === Environment Setup ===
-Log-Message "Setting up directories and environment variables..."
+Write-Host "Set up build directories, code page, vcpkg command line location and environment variables...."
+chcp 65001
 $Env:VSCMD_ARG_HOST_ARCH = "x64"
 $Env:VSCMD_ARG_TGT_ARCH = "x64"
 $BaseDir = "C:\test"
