dcac5998b97ae91468e7a1e465546f0b326e28e3
D Hoyt <xss@xss.cx>
2025-12-10 07:45:46 -0500

    Update iccApplyNamedCmm.cpp

--- Tools/CmdLine/IccApplyNamedCmm/iccApplyNamedCmm.cpp
+++ Tools/CmdLine/IccApplyNamedCmm/iccApplyNamedCmm.cpp
@@ -67,10 +67,10 @@
  // -Initial implementation by Max Derhak 5-15-2003
  // -Modification to support iccMAX by Max Derhak in 2014
  // -Addition of JSON configuraiton by Max Derhak in 2024
- // TEST
+ // 
  //////////////////////////////////////////////////////////////////////
 
-
+#include "IccSignatureUtils.h"
 #include "IccCmm.h"
 #include "IccUtil.h"
 #include "IccDefs.h"
